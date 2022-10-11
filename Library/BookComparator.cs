e765d17e30d2907e88a9859f8e2d8b548e451299
Lyubomir Lalov <ldlalov@hotmail.com>
2022-10-11 13:50:46 +0300

    Add project files.

---
+++ Library/BookComparator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class BookComparator : Comparer<Book>
+    {
+        public override int Compare(Book x, Book y)
+        {
+            int result = x.Title.CompareTo(y.Title);
+            if (result == 0)
+            {
+                result = - x.Year.CompareTo(y.Year);
+            }
+            return result;
+        }
+    }
+}
---
+++ Library/Library.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        public IEnumerator<Book> GetEnumerator()
+        {
+            books.Sort();
+            for (int i = 0; i < books.Count; i++)
+            {
+                yield return books[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private List<Book> books;
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        //public IEnumerator<Book> GetEnumerator()
+        //{
+        //    return new LibraryIterator(books);
+        //}
+
+        //IEnumerator IEnumerable.GetEnumerator()
+        //{
+        //    return GetEnumerator();
+        //}
+
+        //class LibraryIterator : IEnumerator<Book>
+        //{
+        //    private List<Book> books;
+        //    private int currentIndex = -1;
+        //    public LibraryIterator(List<Book> books)
+        //    {
+        //        this.books = books;
+        //        Reset();
+        //    }
+
+        //    public Book Current => books[currentIndex];
+
+        //    object IEnumerator.Current => Current;
+
+        //    public void Dispose()
+        //    {
+        //    }
+
+        //    public bool MoveNext()
+        //    {
+        //        currentIndex++;
+        //        return  currentIndex < books.Count;
+        //    }
+
+        //    public void Reset()
+        //    {
+        //        currentIndex = -1;
+        //    }
+        //}
+    }
+}
